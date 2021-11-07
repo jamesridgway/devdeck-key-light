8cc667ffcd4a54d3f01621a7cc6146e520f60172
Nicolas Dumazet <nicdumz.commits@gmail.com>
2021-11-07 10:43:44 +0100

    Replace on/off images.
    Use the 'lightbulb' material icon, outlined for off, and filled for on.
    
    Icons are from https://fonts.google.com/icons (free under Apache 2.0),
    but have been re-exported as white-on-transparent 512x512 pngs.

--- tests/devdeck_key_light/test_keylight_toggle_control.py
+++ tests/devdeck_key_light/test_keylight_toggle_control.py
@@ -14,7 +14,7 @@ class TestKeyLightToggleControl:
         control = KeyLightToggleControl(0, **settings)
         with mock_context(control) as ctx:
             control.initialize()
-            assert_rendered(ctx, TestingUtils.get_filename('../devdeck_key_light/assets/key-light-on.png'))
+            assert_rendered(ctx, TestingUtils.get_filename('../devdeck_key_light/assets/lightbulb-filled.png'))
 
     @vcr.use_cassette('tests/fixtures/test_key_light_toggle/test_initialize_sets_icon_off.yaml')
     def test_initialize_sets_icon_off(self):
@@ -24,4 +24,4 @@ class TestKeyLightToggleControl:
         control = KeyLightToggleControl(0, **settings)
         with mock_context(control) as ctx:
             control.initialize()
-            assert_rendered(ctx, TestingUtils.get_filename('../devdeck_key_light/assets/key-light-off.png'))
+            assert_rendered(ctx, TestingUtils.get_filename('../devdeck_key_light/assets/lightbulb-outlined.png'))
